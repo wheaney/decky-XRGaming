b17fd6f195c0795d826c75a81cfa473ffd418c2e
wheaney <42350981+wheaney@users.noreply.github.com>
2023-09-01 16:12:36 -0700

    Completely untested WIP, but most of the pieces are there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 # The decky plugin module is located at decky-loader/plugin
 # For easy intellisense checkout the decky-loader code one directory up
@@ -6,36 +7,74 @@ import os
 import decky_plugin
 
 
+CONFIG_FILE_PATH = os.path.join(decky_plugin.DECKY_USER_HOME, ".xreal_driver_config")
+
+
+def parse_boolean(value, default):
+    if not value:
+        return default
+
+    return value.lower() == 'true'
+
+
+def parse_int(value, default):
+    return int(value) if value.isdigit() else default
+
+
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
+    async def retrieve_config(self):
+        config = {}
+        with open(CONFIG_FILE_PATH, 'r') as f:
+            for line in f:
+                key, value = line.strip().split('=')
+                if key in ['disabled', 'use_joystick']:
+                    config[key] = parse_boolean(value, False)
+                elif key == 'mouse_sensitivity':
+                    config[key] = parse_int(value, 20)
+
+        return config
+
+    async def write_config(self, data):
+        with open(CONFIG_FILE_PATH, 'w') as f:
+            for key, value in data.items():
+                if isinstance(value, bool):
+                    f.write(f'{key}={str(value).lower()}\n')
+                elif isinstance(value, int):
+                    f.write(f'{key}={value}\n')
+                elif isinstance(value, list):
+                    f.write(f'{key}={",".join(value)}\n')
+                else:
+                    f.write(f'{key}={value}\n')
+
+    async def is_driver_installed(self):
+        # todo - in addition to checking systemd, store the plugin version in `install_driver` and match it against the current version
+        try:
+            output = subprocess.check_output(['systemctl', 'is-active', 'xreal-air-driver'], stderr=subprocess.STDOUT)
+            return output.strip() == b'active'
+        except subprocess.CalledProcessError:
+            return False
+
+    async def install_driver(self):
+        decky_plugin.logger.info(f"Installing driver for plugin version {decky_plugin.DECKY_PLUGIN_VERSION}")
+
+        # todo store the plugin version in the settings
+        script_path = os.path.dirname(__file__) + "/bin/xreal_driver_setup"
+        binary_path = os.path.dirname(__file__) + "/bin/xrealAirLinuxDriver.tar.gz"
+        try:
+            subprocess.check_output([script_path, binary_path], stderr=subprocess.STDOUT)
+            return True
+        except subprocess.CalledProcessError as exc:
+            decky_plugin.logger.error("Error running setup script", exc)
+            return False
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        decky_plugin.logger.info("Hello World!")
+        pass
 
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
-        decky_plugin.logger.info("Goodbye World!")
         pass
 
     # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky_plugin.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,109 @@
 import {
-  ButtonItem,
-  definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
-  PanelSection,
-  PanelSectionRow,
-  Router,
-  ServerAPI,
-  showContextMenu,
-  staticClasses,
+    ButtonItem,
+    definePlugin,
+    PanelSection,
+    PanelSectionRow,
+    ServerAPI,
+    ServerResponse,
+    SliderField,
+    Spinner,
+    staticClasses,
+    ToggleField,
 } from "decky-frontend-lib";
-import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
+// @ts-ignore
+import React, {Fragment, useEffect, useState, VFC} from "react";
+import {FaGlasses} from "react-icons/fa";
+import {BiMessageError} from "react-icons/bi";
 
-import logo from "../assets/logo.png";
+interface Config {
+    disabled: boolean;
+    use_joystick: boolean;
+    mouse_sensitivity: number;
+}
 
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
+type InstallationStatus = "checking" | "inProgress" | "installed";
+
+const DEFAULT_CONFIG = {disabled: false, use_joystick: false, mouse_sensitivity: 20}
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
-  // const [result, setResult] = useState<number | undefined>();
 
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
+    const [config, setConfig] = useState<Config>(DEFAULT_CONFIG);
+    const [installationStatus, setInstallationStatus] = useState<InstallationStatus>("checking");
+    const [error, setError] = useState<string>();
 
-  return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
+    async function retrieveConfig() {
+        const configRes: ServerResponse<Config> = await serverAPI!.callPluginMethod<null, Config>("retrieve_config", null);
+        configRes.success ? setConfig(configRes.result) : setError(configRes.result);
+    }
 
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
+    async function checkInstallation() {
+        const installedRes: ServerResponse<boolean> = await serverAPI!.callPluginMethod<null, boolean>("is_driver_installed", null);
+        if (installedRes.success) {
+            if (installedRes.result) {
+                setInstallationStatus("installed")
+            } else {
+                setInstallationStatus("inProgress")
+                const installDriverRes = await serverAPI!.callPluginMethod<null, boolean>("install_driver", null);
+                if (installDriverRes.success && installDriverRes.result)
+                    setInstallationStatus("installed")
+                else
+                    setError("Error installing the driver, check the logs")
+            }
+        } else {
+            setError(installedRes.result);
+        }
+    }
 
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>
-    </PanelSection>
-  );
-};
+    async function writeConfig() {
+        const res = await serverAPI!.callPluginMethod<Config, void>("write_config", config);
+        if (!res.success) {
+            setError(res.result);
+            await retrieveConfig();
+        }
+    }
 
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Router.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
+    useEffect(() => {
+        retrieveConfig().catch((err) => setError(err));
+        checkInstallation().catch((err) => setError(err));
+    });
+
+    return (
+        <PanelSection title="XREAL Air Driver">
+            {error && <PanelSectionRow
+                style={{backgroundColor: "pink", borderColor: "red", color: "red"}}><BiMessageError/>{error}
+            </PanelSectionRow>}
+            {!error && <PanelSectionRow>
+                {installationStatus == "installed" && config &&
+                    <Fragment>
+                        <ToggleField checked={config['disabled']}
+                                     label={"Disable"}
+                                     description={"No head-tracking, Air glasses are display-only"}
+                                     onChange={(disabled) => setConfig({...config, disabled})}/>
+                        <ToggleField checked={config['use_joystick']}
+                                     disabled={config['disabled']} label={"Output as joystick"}
+                                     description={"Defaults to mouse movements"}
+                                     onChange={(use_joystick) => setConfig({...config, use_joystick})}/>
+                        <SliderField value={config['mouse_sensitivity']}
+                                     disabled={config['use_joystick'] || config['disabled']} label={"Mouse Sensitivity"}
+                                     onChange={(mouse_sensitivity) => setConfig({...config, mouse_sensitivity})}/>
+                        <ButtonItem label={"Save"} onClick={writeConfig}/>
+                    </Fragment>
+                }
+                {(["checking", "inProgess"].includes(installationStatus) || !config) &&
+                    <Spinner style={{height: '48px'}}/>
+                }
+                {installationStatus == "inProgress" &&
+                    <span>Installing driver...</span>
+                }
+            </PanelSectionRow>}
+        </PanelSection>
+    );
 };
 
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
-
-  return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
-    content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
-    onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
-    },
-  };
+    return {
+        title: <div className={staticClasses.Title}>XREAL Air Driver</div>,
+        content: <Content serverAPI={serverApi}/>,
+        icon: <FaGlasses/>
+    };
 });
