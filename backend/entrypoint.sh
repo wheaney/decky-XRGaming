0c350a59365856ffdb16a49895f6fceeab0b28f6
wheaney <42350981+wheaney@users.noreply.github.com>
2023-09-04 19:22:57 -0700

    Stopgap fix until SteamDeckHomebrew/decky-loader #533 is addressed and widely adopted
    Downloads for the remote_binary files are broken in decky-loader. Since fixing that would only fix installations for users who have updated to the latest loader, this plugin would fail to install for many users. To address this, I've updated my package.json and backend docker entrypoint to use their own custom remote_binary config keys, allowing me to utilize my custom binary logic without the loader attempting file downloads on install.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,8 +5,8 @@ pacman -S --noconfirm git jq
 
 git clone https://github.com/wheaney/decky-xrealAir
 
-# iterate over the remote_binary list in package.json, download and check hashes, and finally copy if matching
-jq -r '.remote_binary[] | "\(.name) \(.sha256hash) \(.url)"' decky-xrealAir/package.json | while read name hash url; do
+# iterate over the custom_remote_binary list in package.json, download and check hashes, and finally copy if matching
+jq -r '.custom_remote_binary[] | "\(.name) \(.sha256hash) \(.url)"' decky-xrealAir/package.json | while read name hash url; do
     curl -Ls "$url" -o "/tmp/$name"
     computed_hash=$(sha256sum "/tmp/$name" | cut -d ' ' -f 1)
     if [ "$computed_hash" == "$hash" ]; then
